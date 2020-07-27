9f9d8933b20ad582432cd73aa0e4e70e02ba5ab7
bways33 <68847203+bways33@users.noreply.github.com>
2020-07-27 17:13:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCMH3rLx4jUFx3zPuxM82K3OVn8e-M2wqs",
+  authDomain: "instagramverfiyuse.firebaseapp.com",
+  databaseURL: "https://instagramverfiyuse.firebaseio.com",
+  projectId: "instagramverfiyuse",
+  storageBucket: "instagramverfiyuse.appspot.com",
+  messagingSenderId: "928022637518",
+  appId: "1:928022637518:web:cd8a9621e19ad04a27d99f",
+  measurementId: "G-GM6VX65XJD"
 };
 
      
